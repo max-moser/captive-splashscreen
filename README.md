66db3b7da422d81d019e2996bfb5764345bc51b1
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2023-05-28 01:37:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Captive splash screen
+
+A `bootstrap`-based splash screen for captive portals such as `nocatsplash` or `nodogsplash`.
+
+
+## Build
+
+The project can be built with the follwing commands:
+
+1. `npm install`
+2. `npm run build`
+
+The finished artifacts are stored in `dist/`.
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/js/main.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    port: 8080,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        mimetype: "image/svg+xml",
+        scheme: "data",
+        type: "asset/resource",
+        generator: {
+          filename: "icons/[hash].svg",
+        },
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // extracts CSS for each JS file that includes CSS
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            // interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: "css-loader",
+          },
+          {
+            // loader for webpack to process CSS with PostCSS
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: () => [autoprefixer],
+              },
+            },
+          },
+          {
+            // loads a SASS/SCSS file and compiles it to CSS
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
