98cc4b24dc8b3fe0fd56e93c1ed6af7e4511b7f5
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2023-07-02 19:45:41 +0200

    Implement modernized version of our current captive portal

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,2 @@
 // import our custom CSS
 import "../scss/styles.scss";
-
-// import all of bootstrap's JS
-import * as bootstrap from "bootstrap";
-
-const button = document.getElementById("button");
-button.addEventListener("click", () => {
-  alert("dang, you actually clicked me");
-});
